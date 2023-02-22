05e2cd9ecd8a2fc6a4619818f438468ae4155db5
= <=>
2023-02-21 23:48:28 -0500

    Frontend-Backend Connected
    WIP:'Frontend' and Solved the all problem of github except one

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import AllRoutes from "./AllRoutes";
+import ReactDOM from "react-dom/client";
+
+// ReactDOM.render(<AllRoutes />, document.getElementById("root"));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AllRoutes />);
